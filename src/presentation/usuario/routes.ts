35c67f00eb35d4268845b83bff1f72d34f49bf4b
Kevin Rodriguez <kevin.rodriguez5@catolica.edu.sv>
2025-10-01 18:41:12 -0600

    feat: add user retrieval by name, surname, and role ID with validation

--- src/presentation/usuario/routes.ts
+++ src/presentation/usuario/routes.ts
@@ -3,6 +3,9 @@ import {
     createUsuarioValidators,
     deleteUsuarioValidators,
     getUsuarioByIdValidators,
+    getUsuariosByApellidoValidators,
+    getUsuariosByNombreValidators,
+    getUsuariosByRolIdValidators,
     updateUsuarioValidators,
 } from "../../middlewares/usuarios.validator";
 import { runValidations } from "../../middlewares/validator";
@@ -14,6 +17,12 @@ export class UsuarioRoutes {
         const router = Router();
         const usuarioController = new UsuarioController();
 
+        // consultas específicas
+        router.get('/nombre/:nombre', runValidations(getUsuariosByNombreValidators), usuarioController.getUsuariosByNombre);
+        router.get('/apellido/:apellido', runValidations(getUsuariosByApellidoValidators), usuarioController.getUsuariosByApellido);
+        router.get('/rol/:rolId', runValidations(getUsuariosByRolIdValidators), usuarioController.getUsuariosByRolId);
+        
+        // CRUD básico
         router.get('/', usuarioController.getAllUsuarios);
         router.get('/:id', runValidations(getUsuarioByIdValidators), usuarioController.getUsuarioById);
         router.post('/', runValidations(createUsuarioValidators), usuarioController.postUsuario);
